5d6547e93b35c897e52dd35993e76ca83d5cac3b
xDarkWhite <45669555+xDarkWhite@users.noreply.github.com>
2022-01-13 20:23:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,14 @@
 from channelmanagerlib import channel_manager
 import setuptools
-a = channel_manager()
 
 setuptools.setup(
     name="channelmanagerlib",
-    version="1.4.3",
+    version="1.0",
     author="Kunode",
     author_email="kunodedev@gmail.com",
     description="API for Channel Manager LIB",
     long_description="negro",
     long_description_content_type="text/markdown",
     url="https://github.com/xDarkWhite/channelmanagerlib",
-    project_urls={
-        "Tracker": "https://github.com/xDarkWhite/channelmanagerlib",
-    },
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Operating System :: OS Independent",
-    ],
-    install_requires=[
-        "requests",
-    ],
     packages=setuptools.find_packages(),
 )
